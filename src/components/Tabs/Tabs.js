4e31c164baffb536177dc86e7151d3f8aca075b5
Sam Levy <slevy239@gmail.com>
2020-02-03 22:06:40 -0500

    mui tabs

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,161 +1,133 @@
-import React, { Component } from 'react';
-import { Tabs, Tab, Grid, Cell, Card, CardTitle, CardText, CardActions, Button, CardMenu, IconButton } from 'react-mdl';
-import './react-tabs.css'
+import React from 'react';
+import PropTypes from 'prop-types';
+import { makeStyles } from '@material-ui/core/styles';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
 
-class TabContent extends Component {
-    constructor(props) {
-        super(props);
-        this.state = { activeTab: 0 };
-    }
+function TabPanel(props) {
+    const { children, value, index, ...other } = props;
 
-    toggleCategories() {
-
-        if (this.state.activeTab === 0) {
-            return (
-                <div style={{ width: '80%', margin: 'auto' }}>
-                    <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                        <CardText>
-                            <p className='bio'>Motivated Full Stack Web Developer with a strong background in communications. Brings hard work
-                                        and dedication inspired by encountering problems and finding an effective solution. Strong
-                                        collaboration skills from previous professional experience. Excels at customer service and
-                        finding solutions to every situation in the workplace.</p>
-                        </CardText>
-                        <CardMenu style={{ color: '#fff' }}>
-                            <IconButton name="share" />
-                        </CardMenu>
-                    </Card>
-
-                </div>
-
-
-            )
-        } else if (this.state.activeTab === 1) {
-            return (
-                <div>
-                    <Grid className='grid'>
-                        <Cell col={4}>
-
-                            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                                <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >Pateint First</CardTitle>
-                                <CardText>
-
-                                </CardText>
-                            </Card>
-                        </Cell>
-                        <Cell col={4}>
-                            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                                <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #1</CardTitle>
-                                <CardText>
-
-                                </CardText>
-                            </Card>
-                        </Cell>
-                        <Cell col={4}>
-
-                            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                                <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #1</CardTitle>
-                                <CardText>
-
-                                </CardText>
-                            </Card>
-                        </Cell>
-                        <Cell col={4}>
-                            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                                <CardTitle style={{ color: '#fff', height: '176px', background: 'url(https://xtnotes-1255646395.coshk.myqcloud.com/images/react-1.svg) center / cover' }} >React Project #1</CardTitle>
-                                <CardText>
-
-                                </CardText>
-                            </Card>
-                        </Cell>
-                    </Grid>
-                </div>
-            )
-        } else if (this.state.activeTab === 2) {
-            return (
-                <div><h1>Experience</h1>
-                    <Grid>
-                        <Cell col={4}>
-                            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                                <CardTitle>
-                                    June 2017 – Current
-                        </CardTitle>
-                                <CardText>
-                                    <h3>Crowded Castle Brewing Company</h3>
-                                    <h4>Server/Bartender</h4>
-                                    <p>Promote, manage and book musicians for private events. Track inventory with Microsoft Excel.</p>
-                                </CardText>
-                            </Card>
-                        </Cell>
-                        <Cell col={4}>
-                            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                                <CardTitle>
-                                    October 2018 - May 2019
-                        </CardTitle>
-                                <CardText>
-                                    <h3>Quench USA Inc.</h3>
-                                    <h4>scheduling Specialist</h4>
-                                    <p>Handled technician scheduling for large metropolitan markets around the U.S. Primarily used
-                                        spreadsheets to handle and manage schedule data. Worked in groups to tackle appointment backlog
-                                        across multiple markets. Relayed information about products to technicians and customers. Gave
-                                customer support when issues were escalated beyond the technician.</p>
-                                </CardText>
-                            </Card>
-                        </Cell>
-                        <Cell col={4}>
-                            <Card shadow={5} style={{ minWidth: '450', margin: 'auto' }}>
-                                <CardTitle>
-                                    September 2012 – May 2016
-                        </CardTitle>
-                                <CardText>
-                                    <h3>Paoli Pharmacy</h3>
-                                    <h4>Pharmacy Technician</h4>
-                                    <p>
-                                        Managed inventory and prescriptions through pharmacy software. Assisted pharmacist with filling
-                                        prescriptions. Aided customers with over-the-counter products. Built relationships with return
-                                patrons. Learned fundamental customer service and communication skills.</p>
-                                </CardText>
-                            </Card>
-                        </Cell>
-                    </Grid>
-                </div>
-            )
-        } else if (this.state.activeTab === 3) {
-            return (
-                <div><h1>Education</h1></div>
-            )
-        } else if (this.state.activeTab === 4) {
-            return (
-                <div><h1>Contact info</h1></div>
-            )
-        }
-
-    }
-
-
-
-    render() {
-        return (
-            <div>
-                <Tabs activeTab={this.state.activeTab} onChange={(tabId) => this.setState({ activeTab: tabId })} ripple>
-                    <Tab>About Me</Tab>
-                    <Tab>Applications</Tab>
-                    <Tab>Experience</Tab>
-                    <Tab>Education</Tab>
-                    <Tab>Contact Information</Tab>
-                </Tabs>
-
-
-                <Grid>
-                    <Cell col={12}>
-                        <div className="content">{this.toggleCategories()}</div>
-                    </Cell>
-                </Grid>
-
-
-            </div>
-        )
-    }
+    return (
+        <Typography
+            component="div"
+            role="tabpanel"
+            hidden={value !== index}
+            id={`simple-tabpanel-${index}`}
+            aria-labelledby={`simple-tab-${index}`}
+            {...other}
+        >
+            {value === index && <Box p={3}>{children}</Box>}
+        </Typography>
+    );
 }
 
-export default TabContent;
+TabPanel.propTypes = {
+    children: PropTypes.node,
+    index: PropTypes.any.isRequired,
+    value: PropTypes.any.isRequired,
+};
+
+function a11yProps(index) {
+    return {
+        id: `simple-tab-${index}`,
+        'aria-controls': `simple-tabpanel-${index}`,
+    };
+}
+
+const useStyles = makeStyles(theme => ({
+    root: {
+        flexGrow: 1,
+        backgroundColor: theme.palette.background.paper,
+        minWidth: 275,
+    },
+    bullet: {
+        display: 'inline-block',
+        margin: '0 2px',
+        transform: 'scale(0.8)',
+    },
+    title: {
+        fontSize: 14,
+    },
+    pos: {
+        marginBottom: 12,
+    },
+}));
+
+export default function SimpleTabs() {
+    const classes = useStyles();
+    const [value, setValue] = React.useState(0);
+
+    const handleChange = (event, newValue) => {
+        setValue(newValue);
+    };
+
+    return (
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Tabs value={value} onChange={handleChange} aria-label="simple tabs example" centered>
+                    <Tab label="About Me" {...a11yProps(0)} />
+                    <Tab label="Applications" {...a11yProps(1)} />
+                    <Tab label="Experience" {...a11yProps(2)} />
+                    <Tab label="Education" {...a11yProps(3)} />
+                    <Tab label="Contact Information" {...a11yProps(4)} />
+
+
+                </Tabs>
+            </AppBar>
+            <TabPanel value={value} index={0}>
+                <Card className={classes.root}>
+                    <CardContent>
+                        <Typography variant="h5" component="h2">About Me</Typography>
+                        <hr></hr>
+                        <br></br>
+                        <Typography>
+                            Motivated Full Stack Web Developer with a strong background in communications. Brings hard work
+                        and dedication inspired by encountering problems and finding an effective solution. Strong
+                        collaboration skills from previous professional experience. Excels at customer service and
+                        finding solutions to every situation in the workplace.
+                        </Typography>
+                    </CardContent>
+                </Card>
+            </TabPanel>
+            <TabPanel value={value} index={1}>
+                <Grid container spacing={3}>
+                    <Card className={classes.root}>
+                        <CardContent>
+                            <Typography>Hello</Typography>
+                        </CardContent>
+                    </Card>
+                    <Card className={classes.root}>
+                        <CardContent>
+                            <Typography>Hello</Typography>
+                        </CardContent>
+                    </Card>
+                    <Card className={classes.root}>
+                        <CardContent>
+                            <Typography>Hello</Typography>
+                        </CardContent>
+                    </Card>
+                </Grid>
+
+            </TabPanel>
+            <TabPanel value={value} index={2}>
+
+            </TabPanel>
+            <TabPanel value={value} index={3}>
+                Education
+      </TabPanel>
+            <TabPanel value={value} index={4}>
+                Contact
+      </TabPanel>
+        </div>
+    );
+}
